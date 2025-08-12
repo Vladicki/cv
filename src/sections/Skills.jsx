ccb727d8087dfa08c1c86e02e8c0201945840e28
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-08-12 16:28:19 +0400

    before implementing Skills section

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,9 +1,4 @@
 import { experiences } from "../constants";
-import HeroExperience from "./HeroExperience"
-import gsap from 'gsap'
-import { useGSAP } from "@gsap/react"
-import { tech } from "../constants"
-import { useState, useRef, useEffect } from "react"
 
 const Skills = () => {
     return (
@@ -11,16 +6,6 @@ const Skills = () => {
             <div className="w-full h-full md:px-10 px-5">
                 <p>Skill Paf</p>
             </div>
-            <figure>
-                <div className="hero-3d-layout">
-                    {/* NEW: Pass the pointerOutTimeoutRef to the HeroExperience component */}
-                    <HeroExperience
-                        setTechDescription={setTechDescription}
-                        pointerOutTimeoutRef={pointerOutTimeoutRef}
-                    />
-                </div>
-            </figure>
-
         </section>
     );
 };
