c73c324553aba600e3fb3f63454ae5c921494b72
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-09-01 16:31:57 +0400

    fixed hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -35,8 +35,8 @@ const Hero = ({ techDescription, setTechDescription, pointerOutTimeoutRef }) => 
                 {/* <img src="/images/bg.png" alt="background" /> */}
             </div>
             <div className="hero-layout">
-                <header className="flex flex-col md:flex-row md:justify-between md:items-start md:w-full w-screen xl:mt-20 mt-2 xl:px-20  px-8 gap-y-7 gap-x-0 sm:gap-x-10 md:gap-x-20">
-                    <div className="flex flex-col ml-10 md:ml-0 gap-7">
+                <header className="flex flex-col md:flex-row md:justify-between md:items-start md:w-full w-screen xl:mt-20 mt-2 xl:px-2 px-4 gap-y-7 gap-x-0 sm:gap-x-10 md:gap-x-20">
+                    <div className="flex flex-col ml-4 md:ml-2 gap-7">
                         <div className="hero-text">
                             <h1>Vladislav Iurev</h1>
                             <h1>Software Developer</h1>
