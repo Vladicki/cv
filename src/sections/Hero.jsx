b8a203e145f5b5dc01a5dba37ee099c06761ca23
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-09-27 23:24:46 +0100

    small margin edit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = ({ techDescription, setTechDescription, pointerOutTimeoutRef }) => 
             </div>
             <div className="hero-layout">
                 <header className="flex flex-col md:flex-row md:justify-between md:items-start md:w-full w-screen xl:mt-20 mt-2 xl:px-2 px-4 gap-y-7 gap-x-0 sm:gap-x-10 md:gap-x-20">
-                    <div className="flex flex-col ml-4 md:ml-2 gap-7">
+                    <div className="flex flex-col ml-2 md:ml-8 gap-7">
                         <div className="hero-text">
                             <h1>Vladislav Iurev</h1>
                             <h1>Software Developer</h1>
